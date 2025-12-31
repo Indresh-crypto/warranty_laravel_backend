74a92a880e32034e243fa575199487f4b01cb747
root <root@goelectronix.pro>
2025-12-31 12:49:55 +0000

    Test aaPanel auto deploy

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,6 +12,7 @@ class AdminController extends Controller
   
 public function login(Request $request)
 {
+    
     $validator = Validator::make($request->all(),[
         'username' => 'required_without:email',
         'email' => 'required_without:username',
